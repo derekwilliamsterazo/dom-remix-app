7280bd8762a8108d33237431b939091f5e290516
Derek Williams <derek.williams@terazo.com>
2024-07-19 10:25:56 -0400

    cleaned up Typograpy style guide

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,7 +6,7 @@ const defaultTheme = require('tailwindcss/defaultTheme');
 
 export default {
   content: [
-    "./app/**/*.{js,jsx,ts,tsx}",
+    "./app/**/*.{js,jsx,ts,tsx,mdx}",
     "./app/components/**/*.{js,jsx,ts,tsx}",
     "./stories/**/*.{js,jsx,ts,tsx}",
   ],
@@ -24,9 +24,9 @@ export default {
       'base': ['1rem', { lineHeight: '1.25rem',letterSpacing: '0.025em', }],
       'sm': ['0.875rem', { lineHeight: '1.25rem',letterSpacing: '0.009375em' }],
       'xs': ['0.625rem', { lineHeight: '0.75rem',letterSpacing: '0.05em' }],
-      'micro': ['0.375rem', { lineHeight: '0.5rem',letterSpacing: '0.05em' }],
+      'xxs': ['0.375rem', { lineHeight: '0.5rem',letterSpacing: '0.05em' }],
       
-      '2xl-number':['2rem', { lineHeight: '1.75rem',letterSpacing: '-0.01em' }],
+      '2xl-number':['2rem', { lineHeight: '2.25rem',letterSpacing: '-0.01em' }],
       'xl-number':['1.5rem', { lineHeight: '1.75rem',letterSpacing: '-0.01em' }],       
       
 
