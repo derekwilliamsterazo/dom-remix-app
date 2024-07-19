a773d3cf7ba128075d0b5ccf334e2a8254a9709a
Derek Williams <derek.williams@terazo.com>
2024-07-19 01:08:09 -0400

    set type in tailwind,css, converted fonts to woff,woff2

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,8 @@
 import type { Config } from "tailwindcss";
 
 const colors = require('tailwindcss/colors');
-// console.log(colors);
+const defaultTheme = require('tailwindcss/defaultTheme');
+//console.log(fonts.fontFamily.mono);
 
 export default {
   content: [
@@ -10,6 +11,26 @@ export default {
     "./stories/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
+    fontSize: {
+      'head1': ['2rem', { lineHeight: '2.5rem',letterSpacing: '-0.01em',fontWeight: '600', }],
+      'head2': ['1.5rem', { lineHeight: '2rem',letterSpacing: '-0.01em',fontWeight: '600', }],
+      'head3': ['1.25rem', { lineHeight: '1.75rem',letterSpacing: '-0.01em',fontWeight: '500', }],
+      'head4': ['1rem', { lineHeight: '1.5rem',letterSpacing: '-0.01em',fontWeight: '600', }],
+      'head5': ['1rem', { lineHeight: '1.5rem',letterSpacing: '-0.01em',fontWeight: '500', }],
+      'head6': ['0.875rem', { lineHeight: '1.25rem',letterSpacing: '-0.01em',fontWeight: '500', }],
+      '3xl': ['3rem', { lineHeight: '3.5rem',letterSpacing: '-0.01em',fontWeight: '600', }],
+
+      'lg': ['1.25rem', { lineHeight: '1.5rem',letterSpacing: '0.075em' }],      
+      'base': ['1rem', { lineHeight: '1.25rem',letterSpacing: '0.025em', }],
+      'sm': ['0.875rem', { lineHeight: '1.25rem',letterSpacing: '0.009375em' }],
+      'xs': ['0.625rem', { lineHeight: '0.75rem',letterSpacing: '0.05em' }],
+      'micro': ['0.375rem', { lineHeight: '0.5rem',letterSpacing: '0.05em' }],
+      
+      '2xl-number':['2rem', { lineHeight: '1.75rem',letterSpacing: '-0.01em' }],
+      'xl-number':['1.5rem', { lineHeight: '1.75rem',letterSpacing: '-0.01em' }],       
+      
+
+    },
     colors: {
       white:'#fff',
       primary: {
@@ -20,7 +41,13 @@ export default {
       secondary: colors.yellow['100'],
       neutral: colors.gray,
     },
-    extend: {},
+    extend: {
+      fontFamily: {
+        mono:['"RedHatMonoRegular"', ...defaultTheme.fontFamily.mono],
+        sans:['"RedHatRegular"', ...defaultTheme.fontFamily.sans],
+        body:['"RedHatRegular"', ...defaultTheme.fontFamily.sans]
+      }
+    },
   },
   plugins: [],
 } satisfies Config;
