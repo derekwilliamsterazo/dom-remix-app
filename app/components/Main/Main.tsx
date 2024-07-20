e55e7931c7457efa2f88b40eeafb9c74dcd64c4a
Derek Williams <derek.williams@terazo.com>
2024-07-20 00:09:57 -0400

    darkmode styles

--- app/components/Main/Main.tsx
+++ app/components/Main/Main.tsx
@@ -15,7 +15,7 @@ export interface MainProps {
 
 export const Main = ({slot1,slot2,slot3,slot1Styles,slot2Styles,slot3Styles }: MainProps) => {
     return (
-        <main className=' text-xs font-sans  dark:bg-gray-900 w-full flex justify-center'>
+        <main className=' text-xs font-sans  dark:bg-gray-900 dark:text-gray-200 w-full flex justify-center'>
             <div className='wrapper gap-12 py-8 px-12 flex max-w-7xl w-full items-start justify-between '>
                 
                 <div className={['', slot1Styles].join(' ')}>{slot1}</div>
