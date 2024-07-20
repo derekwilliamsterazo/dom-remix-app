90db146c0a3108fa48c4a692092d12da4bc513db
Derek Williams <derek.williams@terazo.com>
2024-07-20 00:07:58 -0400

    mocked up views

--- app/views/SingleQuantumEdit/SingleQuantumEdit.tsx
+++ app/views/SingleQuantumEdit/SingleQuantumEdit.tsx
@@ -1,8 +1,9 @@
-import React, { Children } from 'react';
+import React from 'react';
 import { Header } from '~/components/Header/Header';
 import { Heading } from '~/components/Heading/Heading';
 import { Main } from '~/components/Main/Main';
 import { Footer } from '~/components/Footer/Footer';
+import { ProgressNav } from '~/components/ProgressNav/ProgressNav';
 
 import { SquareCheckedIcon } from '~/icons/SquareCheckedIcon';
 import { SquareIcon } from '~/icons/SquareIcon';
@@ -18,46 +19,7 @@ export interface SingleQuantumEditProps {
 
 export const SingleQuantumEdit = ({ }: SingleQuantumEditProps) => {
 
-    interface StepProps  {
-        isCompleted?:Boolean;
-        isActive?:Boolean;
-        children?:any
-    }
-
-    const Step = ({isCompleted, isActive, children}: StepProps) => {
-       
-        const showActive = isActive? 'bg-white text-primary-700':'bg-transparent';
-        return (
-            <div className={['flex rounded-lg p-2 gap-2 text-sm', showActive].join(' ')}>            
-                {isCompleted ? <SquareCheckedIcon/> : <SquareIcon/>}         
-                <span>{children}</span>
-            </div>
-        );
-    };
-
-
-    const ProgressNav = ({}) => { 
     
-
-        return (       
-          <div className=' flex gap-4 flex-col rounded-lg text-white bg-primary-700 w-full  px-4 py-6'>
-            <h2 className='text-head4 font-bold'>Pending Steps to Publish</h2>
-            
-            <Step isActive isCompleted={true} children='Define'/>
-            <Step children='Identify Sources'/>
-
-            <div className="my-1  "><hr className=''/></div>
-            
-    
-            <div className=" flex flex-col gap-4  text-sm ">
-                <a className='' href="" aria-disabled>Overview</a>
-                <a className='' href="" aria-disabled>Forum</a>
-            </div>
-    
-           
-          </div>
-        )
-    }
 
 
 
@@ -87,7 +49,7 @@ export const SingleQuantumEdit = ({ }: SingleQuantumEditProps) => {
         
         slot1={<ProgressNav/>}
         slot1Styles="w-1/5"
-        slot2={Children}
+        slot2="Form Goes Here"
         slot2Styles="bg-gray-200 w-4/5"
       
         />
--- app/views/SingleQuantumView/SingleQuantumView.tsx
+++ app/views/SingleQuantumView/SingleQuantumView.tsx
@@ -11,8 +11,84 @@ export interface SingleQuantumViewProps {
 }
 
 export const SingleQuantumView = ({ }: SingleQuantumViewProps) => {
+    
+    
+    const Overview = ({}) => {
+        return (
+            <>
+                <h2 className='text-head4'>Overview</h2>
+                
+                <div className='py-4'>
+                    <h3 className='text-head3'>Executive Summary</h3>
+                    <p className='text-base'>
+                        The Sales Performance quantum provides data on the performance of sales teams and individual sales representatives. It tracks key metrics such as sales revenue, conversion rates, and customer acquisition costs to provide insights into sales performance.
+                    </p>
+                </div>
+
+                <div className='py-4'>
+                    <h3 className='text-head3'>Source Data</h3>
+                    <p className='text-base'>
+                        This quantum is sourced from the company's CRM system, which tracks sales activities and customer interactions. It also utilizes data from the company's financial system to track revenue and expenses related to sales.
+                    </p>
+                </div>
+
+                <div className='py-4'>
+                    <h3 className='text-head3'>Access Information</h3>
+                    <p className='text-base'>
+                        To access this quantum, users must have appropriate permissions and credentials for the company's CRM and financial systems. Security protocols must also be followed to protect sensitive sales data.
+                    </p>
+                </div>
+
+                <div className='py-4'>
+                    <h3 className='text-head3'>Drilldown to API</h3>
+                    <p className='text-base'>
+                        The Sales Performance quantum can be accessed through the company's API, allowing for real-time data retrieval and analysis.
+                    </p>
+                </div>
+
+                <div className='py-4'>
+                    <h3 className='text-head3'>Data Schema</h3>
+                    <p className='text-base'>
+                        The data schema for this quantum includes fields such as sales revenue, number of sales, conversion rates, and customer acquisition costs. It also includes data on individual sales representatives, such as their performance and targets.
+                    </p>
+                </div>
+
+                <div className='py-4'>
+                    <h3 className='text-head3 '>Metrics</h3>
+                    <p className='text-base'>
+                        Key metrics provided by this quantum include:  
+                        total sales revenue, average conversion rate, and individual sales representative performance. These metrics can be used to track sales performance over time and make data-driven decisions to improve sales strategies.
+                    </p>
+                </div>
+                
+
+                
+
+
+            
+            
+            
+            
+            </>
+        )
+    }
+
+    const Meta = ({}) => {
+        return (
+            <>
+                <div className='py-4'>
+                    <h3 className='text-head5'>Data Contributors</h3>
+                    #Add Profile Card Components
+                </div>
+            </>
+        )
+    }
+
+
+
+
     return (
-        <>
+        <div className='relative'>
        
         <Header
             onCreateAccount={() => {}}
@@ -26,23 +102,23 @@ export const SingleQuantumView = ({ }: SingleQuantumViewProps) => {
         <Heading
         onPrimaryClick={() => {}}
         onSecondaryClick={() => {}}
-        primaryLabel="Primary Action"
-        secondaryLabel="Secondary Action"
-        title="This is the Heading Title"
+        primaryLabel="Follow"
+        secondaryLabel="Request Access"
+        title="Sales Performance"
+        
         />
 
         <Main
-        slot1="Slot 1"
-        slot1Styles="bg-gray-200 w-1/5 h-full"
-        slot2="Slot 2"
-        slot2Styles="bg-gray-200 w-3/5 h-full"
-        slot3="Slot 3"
-        slot3Styles="bg-gray-200 w-1/5 h-full"
+        slot1={<Overview/>}
+        slot1Styles="w-4/5 "
+        slot2={<Meta/>}
+        slot2Styles=" w-1/5 "
+        
         />
 
 
         <Footer />
-     </>
+     </div>
 
     )
 }
