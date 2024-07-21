d6ab784c1047c07aed18aa71abfb75435aa6296b
Derek Williams <derek.williams@terazo.com>
2024-07-21 16:56:56 -0400

    fixed color styles references

--- app/components/ColorBlock/ColorBlock.tsx
+++ app/components/ColorBlock/ColorBlock.tsx
@@ -8,21 +8,22 @@ export interface ColorBlockProps {
     
     label?: string;
     weight?: string;
+    setColorClass?:string
    
 }
 
 
 
 
-export const ColorBlock = ({ label, weight }: ColorBlockProps) => {
+export const ColorBlock = ({ label, weight, setColorClass }: ColorBlockProps) => {
 
   const setColor = `bg-${label}-${weight}`
     return (
 
         <>         
-         <div className='flex items-center gap-8 w-full px-8 '>
-            <div className='text-sm'>{label}-{weight}</div>
-            <div className={[setColor, 'w-full h-10 py-8 '].join(' ')}>sddsdds</div>
+         <div className='flex items-center gap-4 w-full px-2 my-2 '>
+            <div className='text-sm w-24  h-16  flex flex-col justify-center items-end ' >{label}-{weight}</div>
+            <div className={[setColorClass, 'w-96 h-16 py-8 border-slate-200 border-solid border-x border-y '].join(' ')}></div>
 
          </div> 
         </>
